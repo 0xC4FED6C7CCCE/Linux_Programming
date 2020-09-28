458883e273d95616774aabf54b0cdd9093bdbd99
0xC4FED6C7CCCE <3317231312@qq.com>
2020-09-28 18:32:14 +0800

    modified to gets()

--- week_2/2-4.c
+++ week_2/2-4.c
@@ -3,9 +3,7 @@ int main(){
 	int fd,i;
 	char buf[100];	
 	fd = open("./test.data",O_WRONLY|O_CREAT,0644);
-	for(i=0;i<100;i++){
-	buf[i]=i;
-}
+	gets(buf);
 	printf("process id = %d,i = %d,fd = %d",getpid(),i,fd);
 	i=write(fd,buf,sizeof(buf));
 	printf("after i=%d",i);
